60dedc1ad89462fee3b36c05bec2a0fa8033f4cf
Your Name <you@example.com>
2025-12-02 20:51:11 +0100

    Remove evasion fuzzer

--- fuzzer/binary/prepare-tap.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-HOST_IP=192.168.99.37
-
-sudo tunctl -u ivan
-sudo ip link set tap0 up
-sudo ip addr add $HOST_IP/24 dev tap0
--- fuzzer/emulation/qmp-settings.h
+++
@@ -1,5 +0,0 @@
-#define QMPHOST "localhost"
-#define QMPPORT 4444
-#define BUFSIZE 1024
-#define QMP_CMD_CAPABILITES "{\"execute\":\"qmp_capabilities\"}" 
-#define QMP_CMD_MEMSAVE_FMT "{ \"execute\": \"memsave\", \"arguments\": {\"val\": %ld, \"size\": %lu, \"filename\": \"%s\"} }"
